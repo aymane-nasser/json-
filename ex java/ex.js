f6e058ea3e68fececfd0fdea3d6c142c0cdfa716
aymane-nasser <aymane.nasser7@gmail.com>
2021-03-30 10:29:00 +0100

    json

---
+++ ex java/ex.js
@@ -0,0 +1,16 @@
+var tableau = [ ];
+var N;
+do {
+    N=prompt ("donne nombre")
+}
+while(N <=0)
+ tableau[N];
+ for(var i=0;i<=N-1;i++){
+     tableau[i]=prompt ("donne nombre" + "[" + i +"]");
+ }
+ for(var i=0; i<=N-1; i++)
+ {
+    
+    alert("nombre" + "[" + i+"]" + tableau[i] );
+ }
+
